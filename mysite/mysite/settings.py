032854614f217a5ca663a786edf5fe55b9761c87
yusefamiri <yusef.amiri@tuhh.de>
2019-10-21 21:35:55 +0200

    Einstellungen anpassen
    TIME_ZONE = 'Europe/Berlin'
    LANGUAGE_CODE = 'de-ch'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
